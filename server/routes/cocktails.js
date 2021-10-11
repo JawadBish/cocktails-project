34f31fb4df782b85169bd028b8736edae266546e
Jawad Bish <Jawad.bish@gmail.com>
2021-10-12 01:05:24 +0300

    create cocktail backend done

--- server/routes/cocktails.js
+++ server/routes/cocktails.js
@@ -1,10 +1,14 @@
 import express from 'express';
-import { getAllCocktails } from '../controllers/cocktails.js';
+import { getAllCocktails, createCocktail } from '../controllers/cocktails.js';
 
 const router = express.Router();
 
 //Get ALL COCKTAILS
 router.get('/', getAllCocktails);
+router.get('/:id', getOneCocktail);
+router.post('/', createCocktail);
+router.patch('/:id', auth, updateCocktail);
+router.delete('/:id', auth, deleteCocktail);
 
 
 
