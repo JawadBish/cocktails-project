d8fd7bbe62472a6e9a173616e33d21041ecbf3f2
Jawad Bish <Jawad.bish@gmail.com>
2021-10-14 23:54:12 +0300

    Fix REDUX - install UI-Icons

--- client/src/components/Cocktails/Cocktail/Cocktail.js
+++ client/src/components/Cocktails/Cocktail/Cocktail.js
@@ -1,7 +1,14 @@
 import React from 'react'
-//import useStyles from './styles'
+import useStyles from './styles'
+import { Card, CardActions, CardContent, CardMedia, Button, Typography, ButtonBase } from '@material-ui/core';
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import ThumbUpAltOutlined from '@material-ui/icons/ThumbUpAltOutlined';
+import DeleteIcon from '@material-ui/icons/Delete';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import moment from 'moment';
+
 function Cocktail() {
-    //const classes = useStyles();
+    const classes = useStyles();
 
     return (
         <div>
--- client/src/components/Cocktails/Cocktails.js
+++ client/src/components/Cocktails/Cocktails.js
@@ -1,18 +1,40 @@
 import React from 'react'
 import Cocktail from './Cocktail/Cocktail'
-import  { useSelector } from 'react-redux';
-//import useStyles from './styles'
-const Cocktails = () => {
+import { useSelector } from 'react-redux';
+import { Grid, CircularProgress } from '@material-ui/core';
+import useStyles from './styles'
 
-   // const classes = useStyles();
-        const cocktails = useSelector((state)=> state.cocktails)
-        console.log(cocktails);
+const Cocktails = () => {
+    const { cocktails, isLoading } = useSelector((state) => state.cocktails);
+    const classes = useStyles();
+    console.log(cocktails);
+
+    if (isLoading) {
+        return (<>
+            <CircularProgress />
+        </>)
+    }
+    else {
+        if (cocktails?.length < 1 || cocktails === 'undefined') {
+            return (<>
+                <h1 className={classes.h1} style={{ backgroundColor: '#FFFFFF', color: '#990000' }}>No Cocktails Found</h1>
+            </>)
+        }
+    }
+
     return (
-        <div>
-            <h1>Cocktails</h1>
-        <Cocktail/> 
-        <Cocktail/> 
-        </div>
+        cocktails?.length < 1 ? <CircularProgress /> : (
+            <Grid className={classes.mainContainer} container alignItems="stretch" spacing={3}>
+                {cocktails?.map((cocktail) => (
+                    <Grid key={cocktail._id} item xs={12} sm={12} md={6} lg={4}>
+                        <Cocktail cocktail={cocktail} />
+                    </Grid>
+                ))
+
+                }
+            </Grid>
+
+        )
     )
 }
 
