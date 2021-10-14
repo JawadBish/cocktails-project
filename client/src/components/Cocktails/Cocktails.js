b1867b728446f84ffd62d896fc28171e9a072cb5
Jawad Bish <Jawad.bish@gmail.com>
2021-10-14 13:26:09 +0300

    Update Cocktails.js

---
+++ client/src/components/Cocktails/Cocktails.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Cocktail from './Cocktail/Cocktail'
+import useStyles from './styles'
+function Cocktails() {
+
+    const classes = useStyles();
+
+    return (
+        <div>
+            <h1>Cocktails</h1>
+        <Cocktail/> 
+        <Cocktail/> 
+        </div>
+    )
+}
+
+export default Cocktails
