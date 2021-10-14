ffe21d5166b700eda08eded007999249d262b954
Jawad Bish <Jawad.bish@gmail.com>
2021-10-14 15:04:01 +0300

    redux

--- client/src/App.js
+++ client/src/App.js
@@ -21,7 +21,7 @@ const App = () => {
   return (
     <Container maxwidth="lg">
       <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography className={classes.heading} variant="h2:" align="center">Cocktails</Typography>
+        <Typography className={classes.heading} variant="h2" align="center">Cocktails</Typography>
          <img className={classes.image} src={cocktailsImg} alt="cocktails" height="60" />
       </AppBar>
       <Grow in>
